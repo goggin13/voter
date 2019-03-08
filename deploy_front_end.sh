8444ecb6d64398bdd2d94d38de138e86881191b6
goggin13 <goggin13@gmail.com>
2019-03-07 19:49:35 -0600

    deploy front end

--- deploy_front_end.sh
+++ deploy_front_end.sh
@@ -1,2 +1,6 @@
-source .deployer_credentials
-aws s3 cp voter-front/ s3://voter-front-end/ --acl public-read --recursive --cache-control no-cache
+AWS_ACCESS_KEY_ID = File.read(".aws_access_key_id").chomp
+AWS_SECRET_ACCESS_KEY = File.read(".aws_secret_access_key").chomp
+
+command = "AWS_ACCESS_KEY_ID=#{AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=#{AWS_SECRET_ACCESS_KEY} aws s3 cp voter-front/ s3://voter-front-end/ --acl public-read --recursive --cache-control no-cache"
+puts command
+puts `#{command}`
