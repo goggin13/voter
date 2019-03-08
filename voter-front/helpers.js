2826ac6d493855017712f841cf6270b713e3b3fc
goggin13 <goggin13@gmail.com>
2019-03-07 19:12:00 -0600

    set URL to heroku

--- voter-front/helpers.js
+++ voter-front/helpers.js
@@ -1,5 +1,5 @@
-// var HOST = "https://agile-ridge-67293.herokuapp.com";
-var HOST = "http://localhost:3000"
+var HOST = "https://agile-ridge-67293.herokuapp.com";
+// var HOST = "http://localhost:3000"
 
 function getUrlVars() {
   var vars = {};
