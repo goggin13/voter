ecf418b9a91e1a09923fc16c82cb75d1b61926d0
goggin13 <goggin13@gmail.com>
2019-03-16 11:53:24 -0500

    update deploy front end script

--- deploy_front_end.sh
+++ deploy_front_end.sh
@@ -2,6 +2,14 @@
 AWS_ACCESS_KEY_ID = File.read(".aws_access_key_id").chomp
 AWS_SECRET_ACCESS_KEY = File.read(".aws_secret_access_key").chomp
 
+File.readlines("voter-front/helpers.js").grep(/var HOST = .*localhost:3000/).each do |line|
+  unless line.strip.match(/^\/\/.*/)
+    puts "CANCELLING DEPLOY, uncommented 'HOST=localhost'"
+    puts line
+    abort
+  end
+end
+
 command = "AWS_ACCESS_KEY_ID=#{AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=#{AWS_SECRET_ACCESS_KEY} aws s3 cp voter-front/ s3://voter-front-end/ --acl public-read --recursive --cache-control no-cache"
 puts command
 puts `#{command}`
